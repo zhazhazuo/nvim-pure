834eca3e280867d9c5973496ec210d3392eb1c5e
wangzuo01 <wangzuo01@zuoyebang.com>
2025-03-31 20:11:24 +0800

    feat: Add plugin - obsidian

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,25 @@
+local render_markdown = {
+	"MeanderingProgrammer/render-markdown.nvim",
+	-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {},
+	lazy = false,
+	config = function()
+		require("render-markdown").setup {
+			heading = { position = "inline" },
+			quote = { repeat_linebreak = true },
+			win_options = {
+				showbreak = { default = "", rendered = "  " },
+				breakindent = { default = false, rendered = true },
+				breakindentopt = { default = "", rendered = "" },
+			},
+		}
+	end,
+}
+
+return {
+	render_markdown
+}
