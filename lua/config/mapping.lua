8d44ca654c99216cd6387ea0ac35af9b7a300c10
wangzuo01 <wangzuo01@zuoyebang.com>
2025-03-31 20:54:48 +0800

    chore: key map add close buffers

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -7,3 +7,7 @@ map("n", "<leader>gg", "<cmd> LazyGit <CR>", {
 map("n","<C-Q>", "<cmd> nohlsearch <CR>", {
 	desc = "Remove Search HL"
 })
+
+map('n', "<leader>x", "<cmd> bp|bd#<CR>", {
+	desc = "Close Current Buffer"
+})
