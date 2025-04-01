995fdea7de230299f3c59c77f5e5e4fac711f62c
wangzuo01 <wangzuo01@zuoyebang.com>
2025-04-01 17:13:56 +0800

    chore: Mapping
    add <leader>x and <leader>k

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -5,12 +5,39 @@ map("n", "<leader>gg", "<cmd> LazyGit <CR>", {
 })
 
 map("n", "<C-Q>", "<cmd> nohlsearch <CR>", {
-	desc = "Remove Search HL"
+	desc = "Remove Search HL",
 })
 
-map('n', "<leader>x", "<cmd>close<CR>", {
-	desc = "Close Current Buffer"
+map("n", "<leader>x", function()
+	local filetype = vim.bo.filetype
+	local win_type = vim.fn.win_gettype()
+
+	if win_type == "quickfix" then
+		vim.cmd("cclose")
+	elseif win_type == "loclist" then
+		vim.cmd("lclose")
+		-- Close current buffer (if not special)
+	else
+		vim.cmd("bdelete")
+	end
+end, {
+	desc = "Close Current Buffer",
 })
+
+map(
+	"n",
+	"<leader>k",
+	'<cmd> execute "%bdelete|edit#|bdelete#"<CR>',
+	{ desc = "Only keep current buffer" }
+)
+
+map("n", "<leader>ss", function()
+	require("persistence").load()
+end, {
+	desc = "Reset the Workspace",
+})
+
+map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
 
 map("i", "<C-h>", "<Left>", { desc = "move left" })
 map("i", "<C-l>", "<Right>", { desc = "move right" })
