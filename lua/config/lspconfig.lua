d96fef6367e4a6d774645ae6cdaecae4508d264e
wangzuo01 <wangzuo01@zuoyebang.com>
2025-04-01 14:18:39 +0800

    chore: LSP
    ts_ls for react

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -10,29 +10,17 @@ M.init = function(client)
 	end
 end
 
-local lsp_list = { 'lua_ls', 'ts_ls', 'markdown_oxide' }
+local lsp_list = { "lua_ls", "markdown_oxide" }
 
 for _, lsp in ipairs(lsp_list) do
-	lspconfig[lsp].setup {
-
+	lspconfig[lsp].setup({
 		capabilities = require("blink.cmp").get_lsp_capabilities(M.capabilities),
 		on_init = M.init,
-	}
+	})
 end
 
--- lspconfig.lua_ls.setup({
--- 	settings = {
--- 		Lua = {
--- 			talemetry = {
--- 				enable = false,
--- 			},
--- 		},
--- 	},
--- 	capabilities = M.capabilities,
--- 	on_init = M.init,
--- })
---
--- lspconfig.ts_ls.setup({
--- 	capabilities = M.capabilities,
--- 	on_init = M.init,
--- })
+lspconfig.ts_ls.setup({
+	capabilities = require("blink.cmp").get_lsp_capabilities(M.capabilities),
+	on_init = M.init,
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+})
