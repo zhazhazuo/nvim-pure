824096471a38a87fa2c2cf60f03783da210eed67
wangzuo01 <wangzuo01@zuoyebang.com>
2025-04-01 20:23:45 +0800

    chore: finder
    - remove harpoon
    - add spelunk

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -26,37 +26,25 @@ local telescope = {
 	},
 }
 
--- FIXME: it does not work.
-local harpoon = {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
-
-		harpoon:setup()
-
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
-		end)
-		vim.keymap.set("n", "<C-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
-
-		vim.keymap.set("n", "<C-h>", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<C-t>", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<C-n>", function()
-			harpoon:list():select(3)
-		end)
-	end,
+local spelunk = {
+	{
+		"EvWilson/spelunk.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- For window drawing utilities
+			"nvim-telescope/telescope.nvim", -- Optional: for fuzzy search capabilities
+			"nvim-treesitter/nvim-treesitter", -- Optional: for showing grammar context
+		},
+		config = function()
+			require("spelunk").setup({
+				enable_persist = true,
+			})
+		end,
+	},
 }
+
 
 return {
 	snipe,
-	harpoon,
+	spelunk,
 	telescope,
 }
