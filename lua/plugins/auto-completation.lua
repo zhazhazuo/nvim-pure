b36e869d6141e6fcb1b35d7f8dd407dbd87c52bc
wangzuo01 <wangzuo01@zuoyebang.com>
2025-04-09 11:42:31 +0800

    chore: Blink
    - upgrade to 1.1.0
    - rewrite the config by official doc

--- lua/plugins/auto-completation.lua
+++ lua/plugins/auto-completation.lua
@@ -1,5 +1,3 @@
-local trigger_text = "/"
-
 local lua_snip = {
 	"L3MON4D3/LuaSnip",
 	-- follow latest release.
@@ -22,188 +20,68 @@ local lazy_dev = {
 		},
 	},
 	init = function()
-		require("lazydev").setup {
+		require("lazydev").setup({
 			library = { "nvim-dap-ui" },
-		}
+		})
 	end,
 }
 
 local blink = {
 	"saghen/blink.cmp",
-	enabled = true,
-	version = "*",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		"moyiz/blink-emoji.nvim",
-		"Kaiser-Yang/blink-cmp-dictionary",
-	},
-	opts = function(_, opts)
-		opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-			default = { "lazydev", "lsp", "path", "snippets", "buffer", "emoji", "dictionary", "markdown" },
-			providers = {
-				lazydev = {
-					name = "LazyDev",
-					module = "lazydev.integrations.blink",
-					-- make lazydev completions top priority (see `:h blink.cmp`)
-					score_offset = 100,
-				},
-				markdown = {
-					name = "RenderMarkdown",
-					module = "render-markdown.integ.blink",
-					fallbacks = { "lsp" },
-				},
-				dictionary = {
-					module = "blink-cmp-dictionary",
-					name = "Dict",
-					score_offset = 20, -- the higher the number, the higher the priority
-					enabled = true,
-					max_items = 8,
-					min_keyword_length = 8,
-					opts = {
-						dictionary_directories = {
-							vim.fn.expand "~/.config/nvim/dict", -- where you dictionary is
-						},
-					},
-				},
-				lsp = {
-					name = "lsp",
-					enabled = true,
-					module = "blink.cmp.sources.lsp",
-					-- kind = "LSP",
-					--
-					-- When linking markdown notes, I would get snippets and text in the
-					-- suggestions, I want those to show only if there are no LSP
-					-- suggestions
-					-- Disabling fallbacks as my snippets wouldn't show up
-					-- Enabled fallbacks as this seems to be working now
-					fallbacks = { "snippets", "buffer" },
-					score_offset = 90, -- the higher the number, the higher the priority
-				},
-				path = {
-					name = "Path",
-					module = "blink.cmp.sources.path",
-					score_offset = 25,
-					-- When typing a path, I would get snippets and text in the
-					-- suggestions, I want those to show only if there are no path
-					-- suggestions
-					fallbacks = { "snippets", "buffer" },
-					opts = {
-						trailing_slash = false,
-						label_trailing_slash = true,
-						get_cwd = function(context)
-							return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-						end,
-						show_hidden_files_by_default = true,
-					},
-				},
-				buffer = {
-					name = "Buffer",
-					enabled = true,
-					max_items = 3,
-					module = "blink.cmp.sources.buffer",
-					min_keyword_length = 4,
-					score_offset = 15, -- the higher the number, the higher the priority
-				},
-				snippets = {
-					name = "snippets",
-					enabled = true,
-					max_items = 8,
-					min_keyword_length = 2,
-					module = "blink.cmp.sources.snippets",
-					score_offset = 85, -- the higher the number, the higher the priority
-					transform_items = function(_, items)
-						local col = vim.api.nvim_win_get_cursor(0)[2]
-						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-						local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
-						if trigger_pos then
-							for _, item in ipairs(items) do
-								item.textEdit = {
-									newText = item.insertText or item.label,
-									range = {
-										start = { line = vim.fn.line "." - 1, character = trigger_pos - 1 },
-										["end"] = { line = vim.fn.line "." - 1, character = col },
-									},
-								}
-							end
-						end
-						-- NOTE: After the transformation, I have to reload the luasnip source
-						-- Otherwise really crazy shit happens and I spent way too much time
-						-- figurig this out
-						vim.schedule(function()
-							require("blink.cmp").reload "snippets"
-						end)
-						return items
-					end,
-				},
-				-- https://github.com/moyiz/blink-emoji.nvim
-				emoji = {
-					module = "blink-emoji",
-					name = "Emoji",
-					score_offset = 15,   -- the higher the number, the higher the priority
-					opts = { insert = true }, -- Insert emoji (default) or complete its name
-				},
-			},
-		})
+	dependencies = { "rafamadriz/friendly-snippets", "moyiz/blink-emoji.nvim" },
 
-		opts.cmdline = {
-			enabled = false,
-		}
+	-- use a release tag to download pre-built binaries
+	version = "1.*",
+	opts = {
+		keymap = { preset = "default" },
 
-		opts.snippets = {
-			preset = "luasnip",
-			-- This comes from the luasnip extra, if you don't add it, won't be able to
-			-- jump forward or backward in luasnip snippets
-			-- https://www.lazyvim.org/extras/coding/luasnip#blinkcmp-optional
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
-		}
+		appearance = {
+			nerd_font_variant = "normal",
+		},
 
-		opts.keymap = {
-			preset = "default",
-
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
-
-			["<S-k>"] = { "scroll_documentation_up", "fallback" },
-			["<S-j>"] = { "scroll_documentation_down", "fallback" },
-
-			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-		}
-
-		opts.completion = {
+		completion = {
 			keyword = {
 				range = "full",
 			},
 			list = { selection = { preselect = false, auto_insert = true } },
 			menu = {
 				border = "single",
-        auto_show = function(ctx)
-          return ctx.mode ~= "cmdline"
-        end,
+				auto_show = function(ctx)
+					return ctx.mode ~= "cmdline"
+				end,
 			},
 			documentation = {
 				window = {
 					border = "single",
 				},
+				auto_show = false,
 			},
-		}
+		},
 
-		return opts
-	end,
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer", "emoji" },
+			providers = {
+				cmdline = {
+					enabled = function()
+						return vim.fn.getcmdtype() ~= ":" or not vim.fn.getcmdline():match("^[%%0-9,'<>%-]*!")
+					end,
+				},
+				emoji = {
+					module = "blink-emoji",
+					name = "Emoji",
+					score_offset = 15, -- the higher the number, the higher the priority
+					opts = { insert = true }, -- Insert emoji (default) or complete its name
+				},
+			},
+		},
+
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
 
 return {
 	lazy_dev,
 	lua_snip,
-	blink
+	blink,
 }
