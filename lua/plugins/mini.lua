83641a13cad061e490c492fca89f332219090328
wangzuo01 <wangzuo01@zuoyebang.com>
2025-04-10 19:42:44 +0800

    feat: Mini.nvim

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.ai").setup()
+			require("mini.icons").setup()
+			-- require("mini.surround").setup()
+
+			local hipatterns = require("mini.hipatterns")
+			hipatterns.setup({
+				highlighters = {
+					hex_color = hipatterns.gen_highlighter.hex_color(),
+				},
+			})
+		end,
+	},
+}
