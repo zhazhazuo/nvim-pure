506cdd398ca1436ec734335fa400e31f0ab6b75d
wangzuo01 <wangzuo01@zuoyebang.com>
2025-04-16 17:23:39 +0800

    chore: obsidian disables the nvim.cmp for blink.cmp

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -10,7 +10,7 @@ local img_clip = {
 	},
 	keys = {
 		-- suggested keymap
-		-- { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+		{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
 	},
 }
 
@@ -39,19 +39,9 @@ local obsidian = {
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
 	ft = "markdown",
-	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-	-- event = {
-	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-	--   -- refer to `:h file-pattern` for more examples
-	--   "BufReadPre path/to/my-vault/*.md",
-	--   "BufNewFile path/to/my-vault/*.md",
-	-- },
 	dependencies = {
 		-- Required.
 		"nvim-lua/plenary.nvim",
-
-		-- see below for full list of optional dependencies 👇
 	},
 	opts = {
 		ui = {
@@ -65,6 +55,9 @@ local obsidian = {
 		},
 		attachments = {
 			img_folder = "5-Achive/Assets",
+		},
+		completion = {
+			nvim_cmp = false,
 		},
 	},
 }
