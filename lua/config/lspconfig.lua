f68b2537a25d75384be6aa1a01a9c5f27581c235
wangzuo01 <wangzuo01@zuoyebang.com>
2025-09-20 23:27:22 +0800

    chore: LSPConfig / upgrade

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,5 +1,3 @@
-local lspconfig = require("lspconfig")
-
 local M = {}
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -13,16 +11,16 @@ end
 local lsp_list = { "pyright", "lua_ls", "markdown_oxide", "html", "cssls", "tailwindcss" }
 
 for _, lsp in ipairs(lsp_list) do
-	lspconfig[lsp].setup({
+	vim.lsp.config[lsp] = {
 		capabilities = require("blink.cmp").get_lsp_capabilities(M.capabilities),
 		on_init = M.init,
-	})
+	}
 end
 
 local vue_language_server_path = vim.fn.stdpath("data")
 	.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
-lspconfig.ts_ls.setup({
+vim.lsp.config.ts_ls = {
 	capabilities = require("blink.cmp").get_lsp_capabilities(M.capabilities),
 	on_init = M.init,
 	init_options = {
@@ -35,4 +33,8 @@ lspconfig.ts_ls.setup({
 		},
 	},
 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-})
+}
+
+-- Enable all configured LSP servers
+vim.lsp.enable(lsp_list)
+vim.lsp.enable("ts_ls")
