b56cd78d3359ec4e3050fc025cc3cddbb0eb88f8
wangzuo01 <lplp@johns-MacBook-Pro-2.local>
2025-12-09 10:56:44 +0800

    feat: blade / introduce

---
+++ lua/plugins/blade.lua
@@ -0,0 +1,12 @@
+local better_yank = {
+	"gbprod/yanky.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+}
+
+return {
+	better_yank,
+}
